2912ef2f536f17467f939c7273079f55ccf75e6d
oldlaptop <peterjpiwowarski@gmail.com>
2020-06-21 10:27:51 -0400

    some shortcut implementations

--- trun/trun.tcl
+++ trun/trun.tcl
@@ -12,6 +12,8 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+package require http
+
 package provide trun 0.1
 
 namespace eval trun {
@@ -37,7 +39,7 @@ proc unknown {args} {
 	} elseif {[regexp {^[a-z]+:[^:]} $args]} {
 		# Try it as a shortcut
 		set spec [string map {: { }} $args]
-		set script [list [lindex $spec 0] [lrange spec 1 end]]
+		set script [list [lindex $spec 0] [lrange $spec 1 end]]
 
 		return [namespace eval [namespace current]::shortcuts $script]
 	# otherwise treat it as an exec(n) pipeline
@@ -48,6 +50,51 @@ proc unknown {args} {
 namespace unknown unknown
 
 namespace eval shortcuts {
+	namespace path [concat [namespace path] [namespace parent]]
+
+	# DuckDuckGo search
+	proc dd {query} {
+		openurl https://duckduckgo.com/?[http::formatQuery q $query]
+	}
+
+	# Google search
+	proc gg {query} {
+		openurl https://google.com/search?[http::formatQuery q $query]
+	}
+
+	# Local manpage search; requires mandoc be installed and man(1) support the
+	# -w option
+	proc man {query} {
+		if {[auto_execok mandoc] eq ""} {
+			error "the man: shortcut requires mandoc"
+		}
+
+		regexp {([[:alnum:]\-_:\.]+)(\(([[:digit:]]?[[:lower:]]?)\))?} $query -> name -> section
+		if {$section eq ""} {
+			set src [lindex [exec man -w $name] 0]
+		} else {
+			set src [exec man -w $section $name]
+		}
+
+		set outchan [file tempfile outname tkrunner-man.html]
+		try {
+			exec mandoc -T html $src >@ $outchan
+		} finally {
+			close $outchan
+		}
+		openurl $outname
+	}
+
+	# openports.pl pkgname search
+	proc op {pkgname} {
+		openurl https://openports.pl/search?[http::formatQuery pkgname $pkgname]
+	}
+
+	# Wikipedia search
+	proc wp {query} {
+		openurl https://en.wikipedia.org/w/index.php?[http::formatQuery search $query]
+	}
+
 	proc unknown {shortcut args} {
 		error "no such shortcut $shortcut"
 	}
