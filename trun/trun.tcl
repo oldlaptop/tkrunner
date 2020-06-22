fe0fe20e9f3faca999575fb281959b1b2b5ab812
oldlaptop <oldlaptop654@aol.com>
2020-06-21 22:48:19 -0400

    get version requirements straightened out

--- trun/trun.tcl
+++ trun/trun.tcl
@@ -12,7 +12,8 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-package require http
+# http::quoteString
+package require http 2.9.1
 
 package provide trun 0.1
 
